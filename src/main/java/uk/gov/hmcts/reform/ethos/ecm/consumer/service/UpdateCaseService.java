a63d505043c86747297f852f5906b6e7232bcbe5
Javier Carretero <javi_1986@hotmail.com>
2020-05-19 07:49:30 +0100

    Added queue

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/UpdateCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/UpdateCaseService.java
@@ -1,9 +1,25 @@
 package uk.gov.hmcts.reform.ethos.ecm.consumer.service;
 
+import com.google.gson.reflect.TypeToken;
+import com.microsoft.azure.servicebus.Message;
+import com.microsoft.azure.servicebus.QueueClient;
+import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import com.google.gson.Gson;
+import uk.gov.hmcts.reform.ethos.ecm.consumer.config.QueueClientConfig;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -15,14 +31,20 @@ public class UpdateCaseService {
     private String queueCreateUpdatesName;
 
     @Value("${queue.update-case.listen.connection-string}")
-    private String queueUpdateCaseListenString;
-    @Value("${queue.update-case.send.connection-string}")
     private String queueUpdateCaseSendString;
+    @Value("${queue.update-case.send.connection-string}")
+    private String queueUpdateCaseListenString;
     @Value("${queue.update-case.queue-name}")
     private String queueUpdateCaseName;
 
+    private Gson GSON = new Gson();
 
-    @Scheduled(fixedRate = 100000)
+    @Autowired
+    public UpdateCaseService() {
+
+    }
+
+    //@Scheduled(fixedRate = 100000)
     public void updateCase() {
         log.info("Running updateCase job....");
         log.info("queueCreateUpdatesListenString" + queueCreateUpdatesListenString);
@@ -32,4 +54,57 @@ public class UpdateCaseService {
         log.info("queueUpdateCaseName" + queueUpdateCaseName);
     }
 
+    @Scheduled(fixedRate = 100000)
+    public void sendMessages() throws Exception {
+        QueueClient sendClient = new QueueClient(new ConnectionStringBuilder(queueUpdateCaseSendString, queueUpdateCaseName), ReceiveMode.PEEKLOCK);
+        sendMessagesAsync(sendClient).thenRunAsync(sendClient::closeAsync);
+        sendClient.close();
+    }
+
+    CompletableFuture<Void> sendMessagesAsync(QueueClient sendClient) {
+        List<HashMap<String, String>> data = generateMessageContent();
+
+        List<CompletableFuture> tasks = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++) {
+
+            Message message = generateMessage(Integer.toString(i), data, i);
+
+            tasks.add(
+                sendClient.sendAsync(message).thenRunAsync(() -> {
+                    System.out.printf("\n\tMessage acknowledged: Id = %s", message.getMessageId());
+                }));
+        }
+
+        return CompletableFuture.allOf(tasks.toArray(new CompletableFuture<?>[tasks.size()]));
+    }
+
+    private List<HashMap<String, String>> generateMessageContent() {
+        List<HashMap<String, String>> data =
+            GSON.fromJson(
+                "[" +
+                    "{'name' = 'Einstein', 'firstName' = 'Albert'}," +
+                    "{'name' = 'Heisenberg', 'firstName' = 'Werner'}," +
+                    "{'name' = 'Curie', 'firstName' = 'Marie'}," +
+                    "{'name' = 'Hawking', 'firstName' = 'Steven'}," +
+                    "{'name' = 'Newton', 'firstName' = 'Isaac'}," +
+                    "{'name' = 'Bohr', 'firstName' = 'Niels'}," +
+                    "{'name' = 'Faraday', 'firstName' = 'Michael'}," +
+                    "{'name' = 'Galilei', 'firstName' = 'Galileo'}," +
+                    "{'name' = 'Kepler', 'firstName' = 'Johannes'}," +
+                    "{'name' = 'Kopernikus', 'firstName' = 'Nikolaus'}" +
+                    "]",
+                new TypeToken<List<HashMap<String, String>>>() {}.getType());
+        return data;
+    }
+
+    private Message generateMessage(String messageId, List<HashMap<String, String>> data, int i) {
+        Message message = new Message(GSON.toJson(data.get(i), Map.class).getBytes(StandardCharsets.UTF_8));
+        message.setContentType("application/json");
+        message.setLabel("Scientist");
+        message.setMessageId(messageId);
+        message.setTimeToLive(Duration.ofMinutes(2));
+        System.out.printf("\nMessage sending: Id = %s", message.getMessageId());
+        return message;
+    }
 }
