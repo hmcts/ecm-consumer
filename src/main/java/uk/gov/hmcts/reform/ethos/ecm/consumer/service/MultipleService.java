6eb8e64b2eabaff3bffb4ca3d10be9b1550a5b1a
Javier Carretero <javi_1986@hotmail.com>
2020-05-19 07:23:27 +0100

    Show user details

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/MultipleService.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/MultipleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.client.CcdClient;
 import uk.gov.hmcts.ecm.common.exceptions.CaseCreationException;
+import uk.gov.hmcts.ecm.common.idam.models.UserDetails;
 import uk.gov.hmcts.ecm.common.model.bulk.SubmitBulkEvent;
 import uk.gov.hmcts.reform.ethos.ecm.consumer.tasks.MultipleStateTask;
 import java.time.Duration;
@@ -44,6 +45,8 @@ public class MultipleService {
         String accessToken = authenticateUser();
         //String accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzamduaWxqOHVpMGJjbXVpNTluZjAxdGRoMiIsInN1YiI6Ijk0MmZmNzJlLWVlZmItNGI5Ni05MzY4LWQ5OGVkMTdlMzMxYyIsImlhdCI6MTU4OTgwMTQxMiwiY2FzZS1pZCI6IjExNTc3MDciLCJldmVudC1pZCI6InVwZGF0ZUJ1bGtBY3Rpb24iLCJjYXNlLXR5cGUtaWQiOiJTY290bGFuZF9NdWx0aXBsZXMiLCJqdXJpc2RpY3Rpb24taWQiOiJFTVBMT1lNRU5UIiwiY2FzZS1zdGF0ZSI6IkJ1bGtDYXNlU3RhcnRlZCIsImNhc2UtdmVyc2lvbiI6IjExNTVmZmMwMTU0YmMzZWNkZjQwNDdhOGM2OTZhMTA1Y2JmZjMxZjUiLCJlbnRpdHktdmVyc2lvbiI6NX0.hL3HN9q2GDnDqdZTc_YewvFH0MWMXfQU84TNfK_Huyo";
         log.info("AccessToken: " + accessToken);
+        UserDetails userDetails = userService.getUserDetails(accessToken);
+        log.info("UserDetails: " + userDetails);
         List<SubmitBulkEvent> submitBulkEvents = retrieveMultipleCase(accessToken);
         if (submitBulkEvents != null && !submitBulkEvents.isEmpty()) {
 
