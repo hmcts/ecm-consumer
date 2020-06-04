7f526e33ecb3627b6f9f1484e125e4d729e472a8
Javier Carretero <javi_1986@hotmail.com>
2020-06-04 12:34:11 +0100

    Try sending only payload to merge

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/tasks/MultipleStateTask.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/tasks/MultipleStateTask.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.ethos.ecm.consumer.tasks;
 
 import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.ecm.common.client.CcdClient;
+import uk.gov.hmcts.ecm.common.model.bulk.BulkData;
 import uk.gov.hmcts.ecm.common.model.bulk.SubmitBulkEvent;
 import uk.gov.hmcts.ecm.common.model.ccd.CCDRequest;
+
 import java.io.IOException;
 
 @Slf4j
@@ -27,13 +29,17 @@ public class MultipleStateTask implements Runnable {
     public void run() {
 
         log.info("Waiting: " + Thread.currentThread().getName());
-        String caseId = String.valueOf(submitBulkEvent.getCaseId());
+        //String caseId = String.valueOf(submitBulkEvent.getCaseId());
+        String caseId = "1589444647147690";
         try {
             CCDRequest returnedRequest = ccdClient.startBulkEventForCase(authToken, caseTypeId, jurisdiction, caseId);
             log.info("Renaming Multiple Name");
             //submitBulkEvent.getCaseData().setState(CLOSED_STATE);
-            submitBulkEvent.getCaseData().setBulkCaseTitle("NAME CHANGED");
-            ccdClient.submitBulkEventForCase(authToken, submitBulkEvent.getCaseData(), caseTypeId, jurisdiction, returnedRequest, caseId);
+            SubmitBulkEvent submitBulkEvent1 = new SubmitBulkEvent();
+            submitBulkEvent1.setCaseData(new BulkData());
+            submitBulkEvent1.getCaseData().setBulkCaseTitle("NAME CHANGED AGAIN");
+            log.info("Sending: " + submitBulkEvent1.getCaseData());
+            ccdClient.submitBulkEventForCase(authToken, submitBulkEvent1.getCaseData(), caseTypeId, jurisdiction, returnedRequest, caseId);
         } catch (IOException e) {
             log.error("Error processing bulk pre accept threads");
         }
