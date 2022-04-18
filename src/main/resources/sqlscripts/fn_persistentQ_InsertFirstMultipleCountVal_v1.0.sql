18cb15751b3f441b56ac19c63d2e1733ef65d5f7
gauravchawla <gaurav-chawla@hotmail.com>
2022-04-18 23:26:02 +0100

    ECM-690

---
+++ src/main/resources/sqlscripts/fn_persistentQ_InsertFirstMultipleCountVal_v1.0.sql
@@ -0,0 +1,16 @@
+/* CREATE FUNCTION */
+
+CREATE OR REPLACE FUNCTION fn_persistentQ_InsertFirstMultipleCountVal (p_multipleRef varchar(25)) RETURNS void AS $$
+
+BEGIN
+
+LOCK table multipleCounter;
+INSERT into multipleCounter
+SELECT p_multipleRef, 1
+WHERE
+    not exists (select 1 from multipleCounter where multipleref = p_multipleRef);
+
+COMMIT;
+
+END;
+   $$ LANGUAGE plpgsql;
