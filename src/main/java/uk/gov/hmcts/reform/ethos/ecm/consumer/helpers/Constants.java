b11719b08ce31db92390c7c1b69c9eccaface821
gauravchawla <gaurav-chawla@hotmail.com>
2022-08-12 11:44:56 +0100

    ECM-900: remove email notification

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/helpers/Constants.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/helpers/Constants.java
@@ -7,6 +7,4 @@ public class Constants {
 
     public static final String UNPROCESSABLE_STATE = "Unprocessable State";
     public static final String UNPROCESSABLE_MESSAGE = "Unprocessable Message";
-    public static final String CONFIRMATION_ERROR_EMAIL = "44848223-7d69-436b-b083-2429ed2ae653";
-    public static final String CONFIRMATION_OK_EMAIL = "905ac179-cfe3-40c0-ba33-93323b25f149";
 }
