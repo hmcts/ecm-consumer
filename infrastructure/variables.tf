2e8e29b45bd791f0b23b9828a744d61820dd725b
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-16 13:24:54 +0000

    fix

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -11,7 +11,9 @@ variable "env" {}
 
 variable "subscription" {}
 
-variable "deployment_namespace" {}
+variable "deployment_namespace" {
+  default = ""
+}
 
 variable "common_tags" {
   type = map(string)
