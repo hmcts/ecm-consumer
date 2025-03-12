64d16ee3714c3845494e2b57e844be159074f5ef
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-03-12 17:19:30 +0000

    Migration Cron Job
    Checkstyle
    
    Bumping chart version/ fixing aliases
    
    Change?
    
    Change?
    
    Add missing properties back in
    
    Test
    
    PMD

--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/Application.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/Application.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages =
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
     })
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @Slf4j
+@EnableScheduling
 public class Application {
 
     public static void main(final String[] args) {
--- src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/idam/IdamApi.java
+++ src/main/java/uk/gov/hmcts/reform/ethos/ecm/consumer/idam/IdamApi.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.ethos.ecm.consumer.idam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.ecm.common.idam.models.UserDetails;
 
@@ -10,4 +11,10 @@ import uk.gov.hmcts.ecm.common.idam.models.UserDetails;
 public interface IdamApi {
     @GetMapping(value = "/o/userinfo")
     UserDetails retrieveUserDetails(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation);
+
+    @GetMapping("/api/v1/users/{userId}")
+    UserDetails getUserByUserId(
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+        @PathVariable("userId") String userId
+    );
 }
