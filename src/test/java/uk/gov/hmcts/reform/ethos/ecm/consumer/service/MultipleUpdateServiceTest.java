b9d4f3262b4eaab1ae5db786b124029a927302eb
btensay <tensay.bulcha@gmail.com>
2025-05-06 13:39:54 +0100

    unit tests updated and new ones added

--- src/test/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/MultipleUpdateServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/ethos/ecm/consumer/service/MultipleUpdateServiceTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -206,6 +207,71 @@ public class MultipleUpdateServiceTest {
                                                      eq(updateCaseMsg.getCaseTypeId()),
                                                      eq(updateCaseMsg.getJurisdiction()),
                                                      any(),
+                                                     any());
+    }
+
+    @Test
+    public void sendUpdateToMultipleLogicWithEmptyMultipleErrorsList() throws IOException {
+        when(userService.getAccessToken()).thenReturn(userToken);
+        when(ccdClient.retrieveMultipleCasesElasticSearchWithRetries(anyString(),
+                                                                     anyString(),
+                                                                     anyString())).thenReturn(submitMultipleEvents);
+
+        multipleUpdateService.sendUpdateToMultipleLogic(updateCaseMsg, new ArrayList<>());
+
+        verifyMocks();
+    }
+
+    @Test
+    public void sendUpdateToMultipleLogicWithNonEmptyMultipleErrorsList() throws IOException {
+        when(userService.getAccessToken()).thenReturn(userToken);
+        when(ccdClient.retrieveMultipleCasesElasticSearchWithRetries(anyString(),
+                                                                     anyString(),
+                                                                     anyString())).thenReturn(submitMultipleEvents);
+        var updateCaseMsg2 = Helper.generateCreationSingleCaseMsg();
+        MultipleErrors multipleErrors = new MultipleErrors();
+        multipleErrors.setDescription("Some error");
+        List<MultipleErrors> multipleErrorsList = new ArrayList<>();
+        multipleErrorsList.add(multipleErrors);
+
+        multipleUpdateService.sendUpdateToMultipleLogic(updateCaseMsg2, multipleErrorsList);
+
+        verify(ccdClient).retrieveMultipleCasesElasticSearchWithRetries(eq(userToken),
+                                                                        eq(updateCaseMsg2.getCaseTypeId()),
+                                                                        eq(updateCaseMsg2.getMultipleRef()));
+        verify(ccdClient, times(0)).submitMultipleEventForCase(eq(userToken),
+                                                     any(),
+                                                     eq(updateCaseMsg2.getCaseTypeId()),
+                                                     eq(updateCaseMsg2.getJurisdiction()),
+                                                     any(),
+                                                     any());
+        verifyNoMoreInteractions(ccdClient);
+    }
+
+    @Test
+    public void sendUpdateToMultipleLogicWithNullDataModelParent() throws IOException {
+        when(userService.getAccessToken()).thenReturn(userToken);
+        when(ccdClient.retrieveMultipleCasesElasticSearchWithRetries(anyString(),
+                                                                     anyString(),
+                                                                     anyString()))
+            .thenReturn(submitMultipleEvents);
+
+        updateCaseMsg.setDataModelParent(null);
+
+        multipleUpdateService.sendUpdateToMultipleLogic(updateCaseMsg, new ArrayList<>());
+
+        verify(ccdClient).retrieveMultipleCasesElasticSearchWithRetries(eq(userToken),
+                                                                        eq(updateCaseMsg.getCaseTypeId()),
+                                                                        eq(updateCaseMsg.getMultipleRef()));
+        verify(ccdClient).submitMultipleEventForCase(eq(userToken),
+                                                     any(),
+                                                     eq(updateCaseMsg.getCaseTypeId()),
+                                                     eq(updateCaseMsg.getJurisdiction()),
+                                                     any(),
+                                                     any());
+        verify(ccdClient, times(1)).startBulkAmendEventForCase(eq(userToken),
+                                                     eq(updateCaseMsg.getCaseTypeId()),
+                                                     eq(updateCaseMsg.getJurisdiction()),
                                                      any());
     }
 
